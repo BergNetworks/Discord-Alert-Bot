631d37b1d0f5022b5b3d75ea61b729d775f3a3d7
BergNetworks <22014997+BergNetworks@users.noreply.github.com>
2023-05-21 21:04:36 -0700

    Add script to bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,39 @@
+import discord
+from discord.ext import commands
+from datetime import datetime
+
+#Intent
+intents = discord.Intents.default()
+intents.typing = False
+intents.presences = False
+intents.message_content = True
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+
+
+
+@bot.event
+
+#Send DM on script load
+async def on_ready():
+    user_id = YOUR_USER_ID_HERE  # Replace with the user ID of the user you wish to DM on each script load
+    
+    # Get current date and time
+    now = datetime.now()        
+    current_time = now.strftime("%m/%d/%Y-%I:%M %p") #Time format in standard time (Month/Day/Year - Hour:Minute AM/PM)
+    
+    #Message Content
+    message_content = f"I am online! {current_time}"
+    
+    #Fetch userid
+    user = await bot.fetch_user(user_id)
+    if user:
+        await user.send(message_content)
+        print("Message sent to the user!")
+    else:
+        print("User not found!")
+
+
+
+bot.run('YOUR BOTS SECRET/KEY HERE')
 
